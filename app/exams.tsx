7f69648686518a74819e0878a5333a1a04b053d0
Ashishshankar26 <oneashishshankar@gmail.com>
2026-06-30 19:50:33 +0530

    Fix: Update fallback seating plan EXAMS_URL token in exams.tsx

--- app/exams.tsx
+++ app/exams.tsx
@@ -9,7 +9,7 @@ import * as Sharing from 'expo-sharing';
 import { useScraper } from '../context/ScraperContext';
 import { useTheme } from '../context/ThemeContext';
 
-const EXAMS_URL = 'https://studentums.lpu.in/dashboard/examination/conduct/seatingplan?token=e1832132a1cdb364325a027f9053b98aeb903fee43ae562924a167ca052aec55c2ffa8b58aef6601b4cf54c6674cabf570fef45885a38bf78b32007a3d40a8c5c9c70223136940245df090e86fe1e6c47f6b76c4366976cd672978092b8c8eb2';
+const EXAMS_URL = 'https://studentums.lpu.in/dashboard/examination/conduct/seatingplan?token=9415acec69a60927c3da42e3621985689168636d12a029bc4fdb0f0ebda14e0f98645af63ac38e1f0a87333767b55e3a235b4167efc803e6741b51382284fbb9a095efba7644972725fd72f655b410f1d2a1b3949e6a5534b23bfd70688037a4';
 
 export default function ExamsScreen() {
   const { data } = useScraper();
